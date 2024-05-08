36a985ea07f2bf15ed51f825176f08bb30d7cf8f
PratKakade <101464490+PratKakade@users.noreply.github.com>
2024-05-09 01:55:45 +0530

    Scala/Hadoop

---
+++ first.scala
@@ -0,0 +1,20 @@
+object Ex{
+
+	def main(args: Array[String]){
+	
+		var number= -100;
+		
+		if(number==0){
+			println("number is zero");
+		}
+		else if(number>0){
+			println("number is negative");
+		}
+		
+		else{
+			println("number is negative");
+			
+		}
+		
+	}	
+}
